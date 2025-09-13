5bd500eceebd86859315e0b78630fd30dfbea8a3
homedebian <homedebian@example.com>
2025-09-13 10:14:47 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+use rand::Rng;
+
+fn main(){
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..10);
+
+    println!("The secret number is : {}", secret_number);
+
+    println!("Please inpu your guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("you guessd: {}", guess);
+}
