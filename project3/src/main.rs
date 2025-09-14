607c742a2de3da414ea11a28488e84fc8cfdab7c
homedebian <homedebian@example.com>
2025-09-14 12:23:17 +0900

    add new project

---
+++ project3/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+    function_test();
+}
+
+fn function_test(){
+    println!("this is another function");
+}
---
+++ project3/target/debug/project3.d
@@ -0,0 +1,1 @@
+/home/user/hello-rust/project3/target/debug/project3: /home/user/hello-rust/project3/src/main.rs
