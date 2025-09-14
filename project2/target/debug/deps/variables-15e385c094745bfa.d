d2381140647e192dda477ddba44cece0f3ca8036
homedebian <homedebian@example.com>
2025-09-14 12:31:45 +0900

    del dic

---
+++ project2/target/debug/deps/variables-15e385c094745bfa.d
@@ -0,0 +1,5 @@
+/home/user/hello-rust/project2/target/debug/deps/variables-15e385c094745bfa.d: src/main.rs
+
+/home/user/hello-rust/project2/target/debug/deps/variables-15e385c094745bfa: src/main.rs
+
+src/main.rs:
---
+++ project2/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/user/hello-rust/project2/target/debug/variables: /home/user/hello-rust/project2/src/main.rs
